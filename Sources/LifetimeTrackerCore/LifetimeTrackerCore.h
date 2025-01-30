9a104d55e6ec2188a9663a88dce2f43648f6717a
Martin Čolja <martin.colja@infinum.com>
2025-01-30 18:55:19 +0100

    add new workspace

---
+++ Sources/LifetimeTrackerCore/LifetimeTrackerCore.h
@@ -0,0 +1,18 @@
+//
+//  LifetimeTrackerCore.h
+//  LifetimeTrackerCore
+//
+//  Created by Martin Čolja on 30.01.2025..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LifetimeTrackerCore.
+FOUNDATION_EXPORT double LifetimeTrackerCoreVersionNumber;
+
+//! Project version string for LifetimeTrackerCore.
+FOUNDATION_EXPORT const unsigned char LifetimeTrackerCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LifetimeTrackerCore/PublicHeader.h>
+
+
